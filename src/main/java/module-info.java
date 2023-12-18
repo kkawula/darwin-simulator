5b75f47b2c644a4eae4c154384188e5d3d862d34
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-12-18 17:22:51 +0100

    hot fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module agh.ics.oop.view {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens agh.ics.oop.view  to javafx.fxml;
-    exports agh.ics.oop.view;
     exports agh.ics.oop.presenter;
     opens agh.ics.oop.presenter to javafx.fxml;
 }

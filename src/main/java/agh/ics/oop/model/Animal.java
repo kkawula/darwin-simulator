71a1646975dca2858229d85b0284156f48031a97
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-12-20 13:20:17 +0100

    Updating children and offsprings changes and tests

--- src/main/java/agh/ics/oop/model/Animal.java
+++ src/main/java/agh/ics/oop/model/Animal.java
@@ -1,31 +1,33 @@
 package agh.ics.oop.model;
 
+import java.util.HashSet;
+
 public class Animal {
     private Vector2d position;
     private Animal father;
     private Animal mother;
 
     private int energy;
-    private int age = 0;
+    private final int age = 0;
+    private final int birthDay = 0;
+    private final int deathDay = 0;
+    private final boolean isDead = false;
+
     private int children = 0;
     private int offspring = 0;
-    private int grassEaten = 0;
-    private boolean isDead = false;
+    private final int grassEaten = 0;
     private int genomeLength;
+    public int activeGene = 0;
+    private final Genome genome;
 
-    private Genome genome;
+    public Behavior behavior;
 
-    public Genome getGenome() {
-        return genome;
+
+    public void setBehavior(Behavior behavior) {
+        this.behavior = behavior;
     }
 
-    public int getEnergy() {
-        return energy;
-    }
 
-    public void setEnergy(int energy) {
-        this.energy = energy;
-    }
 
     public Animal (Vector2d newPosition, int energy, int genomeLength) {
         this.position = newPosition;
@@ -38,32 +40,64 @@ public class Animal {
         this.energy = energy;
         this.father = father;
         this.mother = mother;
+        updateChildren();
 
         this.genome = new Genome(father.getEnergy(), mother.getEnergy(), father.getGenome(), mother.getGenome());
     }
 
     public void updateChildren() {
-        children++;
-        offspring++;
+        father.setChildren(father.getChildren() + 1);
+        mother.setChildren(mother.getChildren() + 1);
 
-        if (father != null) {
-            father.updateOffSpring();
-        }
-        if (mother != null) {
-            mother.updateOffSpring();
-        }
+        HashSet<Animal> visitedParents = new HashSet<>();
+        updateOffspring(visitedParents);
 
     }
 
-    public void updateOffSpring() {
-        offspring++;
+    public void updateOffspring(HashSet<Animal> visitedParents) {
 
-        if (father != null) {
-            father.updateOffSpring();
+        if (father != null && !visitedParents.contains(father)) {
+            father.setOffspring(father.getOffspring() + 1);
+            visitedParents.add(father);
+            father.updateOffspring(visitedParents);
         }
-        if (mother != null) {
-            mother.updateOffSpring();
+        if (mother != null && !visitedParents.contains(mother)) {
+            mother.setOffspring(mother.getOffspring() + 1);
+            visitedParents.add(mother);
+            mother.updateOffspring(visitedParents);
         }
+    }
+
+    public void performGeneBehavior() {
+        behavior.geneBehavior(this);
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public int getOffspring() {
+        return offspring;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public void setOffspring(int offspring) {
+        this.offspring = offspring;
+    }
+
+    public Genome getGenome() {
+        return genome;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
     }
 
     public void setPosition(Vector2d position) {
